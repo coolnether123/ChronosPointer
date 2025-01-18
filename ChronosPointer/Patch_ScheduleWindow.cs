63755420309ad0354ae3df90ee0247c11a231f83
Coolnether123 <optimusjared@gmail.com>
2025-01-18 10:05:48 -0600

    Changed offset x from 0 to 1

--- ChronosPointer/Patch_ScheduleWindow.cs
+++ ChronosPointer/Patch_ScheduleWindow.cs
@@ -36,7 +36,7 @@ namespace ChronosPointer
         public static Rect UseMeForTheXYPosOfDayNightBar;
 
         // Where the schedule grid starts
-        private static float BaseOffsetX = 1f;// CalculateBaseOffsetX();
+        private static float BaseOffsetX = 0f;// CalculateBaseOffsetX();
         private const float BaseOffsetY = 40;
 
         // Each hour cell
